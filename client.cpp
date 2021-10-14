83250411e98719c8b3d6dc009f79a8decbe149a8
twt-tec <79842908+twt-tec@users.noreply.github.com>
2021-10-14 12:30:02 +0800

    Add files via upload

--- client.cpp
+++ client.cpp
@@ -1,145 +1,116 @@
-//#include "pch.h"
-#include<iostream>
-#include<winsock.h>
-#include <string>
+#include "communi.h"
+#include <iostream>
 #include <windows.h>
-#pragma comment(lib,"ws2_32.lib")
-using namespace std;
-int B, id, num, flag;
-namespace TWT {
-int send_len = 0;
-int recv_len = 0;
-//定义发送缓冲区和接受缓冲区
-char send_buf[100];
-char recv_buf[100];
-//定义服务端套接字，接受请求套接字
-SOCKET s_server;
-//服务端地址客户端地址
-SOCKADDR_IN server_addr;
-void initialization();
-int init() {
-	//定义长度变量
-	
-	initialization();
-	//填充服务端信息
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
-	server_addr.sin_port = htons(1234);
-	//创建套接字
-	s_server = socket(AF_INET, SOCK_STREAM, 0);
-	if (connect(s_server, (SOCKADDR *)&server_addr, sizeof(SOCKADDR)) == SOCKET_ERROR) {
-		cout << "服务器连接失败！" << endl;
-		WSACleanup();
-	}
-	else {
-		cout << "服务器连接成功！" << endl;
-	}
+#include <vector>
+#include <string>
+constexpr int _Wolf = 1, _Villager = 0;
+twt_client cs;
+int id, B, cha, flag;
+std::vector<int> all;
+
+int to_int(std::string x) {
+	int an = 0;
+	for (char c : x) an = an * 10 + c - '0';
+	return an;
 }
-std::string rec() {
-	recv_len = recv(s_server, recv_buf, 100, 0);
-	if (recv_len < 0) {
-		cout << "接受失败！" << endl;
-		return "haha";
-	}
-	else {
-		std::string an = recv_buf;
-		return an;
-	}
+void erase(int x) {
+	int k = 0;
+	for (int i = 0; i < (int)all.size(); i++)
+		if (all[i] == x) k = i;
+	all.erase(all.begin()+k); 
 }
-void sent(std::string s) {
-	send_len = send(s_server, s.c_str(), 100, 0);
-	if (send_len < 0) {
-		cout << "发送失败！" << endl;
-	}
-	
+std::string to_string(std::vector<int> x) {
+	std::string an;
+	for (int c : x) an = an + to_string(c) + " ";
+	return an; 
 }
-void initialization() {
-	//初始化套接字库
-	WORD w_req = MAKEWORD(2, 2);//版本号
-	WSADATA wsadata;
-	int err;
-	err = WSAStartup(w_req, &wsadata);
-	if (err != 0) {
-		cout << "初始化套接字库失败！" << endl;
-	}
-	else {
-		cout << "初始化套接字库成功！" << endl;
-	}
-	//检测版本号
-	if (LOBYTE(wsadata.wVersion) != 2 || HIBYTE(wsadata.wHighVersion) != 2) {
-		cout << "套接字库版本号不符！" << endl;
-		WSACleanup();
-	}
-	else {
-		cout << "套接字库版本正确！" << endl;
-	}
-	//填充服务端地址信息
- 
+void getDie() {
+	std::string c = cs.rec();
+	std::cout << c << '\n';
+	int p = c.find(' ');
+	int x = to_int(c.substr(0, p));
+	erase(x);
+	if (x == id) std::cout << "YOU DIED!\n", flag = 1;
+	system("pause");
 }
+void print(std::string s) {
+	system("cls");
+	std::cout << "sta : " << s << '\n';
+	std::cout << "players alive : " << to_string(all) << '\n';
+	if (flag) std::cout << "YOU HAVE ALREADY DIED!\n";
+	std::cout << "Waiting for others...\n";
 }
-using namespace TWT;
-void night() {
-	std::cout << "close your eye!\n";
-	if (id && !flag) {
-		std::cout << "wating for other wolf please!\n";
-		rec();
-		std::cout << "now is your turn. input the man that you want to kill\n";
-		int x;
+void vote() {
+	if (flag) return;
+	std::cout << "Vote now!\n> ";
+	int x;
+	std::cin >> x;
+	cs.sent(to_string(x));
+	std::string c = cs.rec();
+	while (c != "ok") {
+		std::cout << c << "\n> ";
 		std::cin >> x;
-		sent(std::string()+char(x+'0'));
+		cs.sent(to_string(x));
+		c = cs.rec();
 	}
-	std::string k = rec();
-	std::cout << k << '\n';
-	B--;
-	if (k.back()-'0' == num) flag = 1;
+}
+void wolf() {
+	if (cha != _Wolf || flag) return;
+	vote();
+}
+void night() {
+	print("night");
+	wolf();
+	getDie();
+}
+void talk() {
+	for (int i = 0; i <= (int)all.size()-flag; i++) {
+		std::string c = cs.rec();
+		if (c == "you") {
+			std::cout << "Write your message now:\n> ";
+			std::string an;
+//			std::cin >> an;
+			std::getline(cin, an);
+			cs.sent(an);
+		}
+		else std::cout << c << '\n';
+	}
 }
 void day() {
-	std::cout << "is morning!\n";
-	std::cout << "wating for other plays\n";
-	for (int i = 0; i <= B; i++) {
-		std::string k = rec();
-		if (k[0] == '-') {
-			std::cout << "is your turn! input what you want to say!\n> ";
-			std::string s;
-			std::cin >> s;
-			sent(s);
-		}
-		else std::cout << k.back()+1 << " says:" << k << '\n';
-	}
-	if (!flag) std::cout << "vote who you want to kill!\n> ";
-	int x;
-	if (!flag) std::cin >> x;
-	if (!flag) sent(std::string()+char(x+'0'));
-	std::cout << "wating for other plays...\n";
-	std::string k = rec();
-	std::cout << k << '\n';
-	B--;
-	if (k.back()-'0' == num) flag = 1;
+	print("morning");
+	talk();
+	vote();
+	getDie();
+}
+void getCha() {
+	std::string c = cs.rec();
+	if (to_int(c) == _Wolf) std::cout << "You are Wolf\n", cha = _Wolf;
+	else std::cout << "You are Villager\n", cha = _Villager;
+	if (cha == _Wolf) c = cs.rec(), std::cout << "wolves are player " + c + '\n'; 
 }
 bool check() {
-	std::string an = rec(); 
-	if (an == "lr") return std::cout << "wolves win", 1;
-	else if (an == "pm") return std::cout << "villagers win", 1;
-	return 0;
+	std::string s = cs.rec();
+	if (s[0] != '&') return false;
+	std::cout << s.substr(1, s.size()-1) << '\n';
+	return true;
 }
 int main() {
-	system("chcp 936");
-	init();
-	std::cout << "wait for other players...\n";
-	std::string c = rec();
-	std::cout << "there are " + c + " players, the game has began!\n";
-	B = c[0]-'0';
-	c = rec();
-	std::cout << "you are player " << c << '\n';
-	c = rec();
-	num = c[0]-'0'+1;
-	if (c == "1") id = 1;
-	std::cout << (std::string)"You are " + (c == "0" ? "villager" : "wolf") << '\n';
+	system("chcp 936"); system("cls");
+	std::cout << "Welcome!\nWaing for other players...\n";
+	cs.init(1234);
+	std::string c = cs.rec();
+	system("cls");
+	std::cout << "There are " << c << " players\n";
+	B = to_int(c);
+	for (int i = 1; i <= B; i++) all.push_back(i);
+	c = cs.rec();
+	id = to_int(c);
+	std::cout << "You are player " << id << "!\n";
+	getCha();
+	system("pause");
 	while (1) {
-		if (!check()) night();
-		else break;
-		if (!check()) day();
-		else break;
+		if (check()) break; night();
+		if (check()) break; day();
 	}
 	system("pause");
 }
--- server.cpp
+++ server.cpp
@@ -1,177 +1,133 @@
-#include <winsock.h>
-
+#include "communi.h"
 #include <iostream>
-#include <thread>
-#include <windows.h>
-#include <string>
 #include <ctime>
 #include <cstdlib>
-//#include "pch.h"
-#pragma comment(lib, "ws2_32.lib")
-using namespace std;
-constexpr int ERR = 2;
+#include <algorithm>
+#include <vector>
+constexpr int _Villager = 0, _Wolf = 1;
+twt_server cs;
 int B;
-int cha[10], die[10];
-struct twt {
-	int send_len = 0;
-	int recv_len = 0;
-	int len = 0;
-	char send_buf[100];
-	char recv_buf[100];
-	//定义服务端套接字，接受请求套接字
-	SOCKET s_server;
-	SOCKET s_accept[2];
-	//服务端地址客户端地址
-	SOCKADDR_IN server_addr;
-	SOCKADDR_IN accept_addr;
-	void initialization() {
-	    //初始化套接字库
-	    WORD w_req = MAKEWORD(2, 2);  //版本号
-	    WSADATA wsadata;
-	    int err;
-	    err = WSAStartup(w_req, &wsadata);
-	    //检测版本号
-	    if (LOBYTE(wsadata.wVersion) != 2 || HIBYTE(wsadata.wHighVersion) != 2) {
-	        cout << "ERR" << endl;
-	        WSACleanup();
-	    } 
-	    //填充服务端地址信息
-	}
-	int init(int x) {
-//		system("chcp 936");
-	    initialization();
-	    //填充服务端信息
-	    server_addr.sin_family = AF_INET;
-	    server_addr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
-	    server_addr.sin_port = htons(x);
-	    //创建套接字
-	    s_server = socket(AF_INET, SOCK_STREAM, 0);
-	    if (bind(s_server, (SOCKADDR *)&server_addr, sizeof(SOCKADDR)) ==
-	        SOCKET_ERROR) {
-	        cout << "ERR" << endl;
-	        WSACleanup();
-	    }
-	    //设置套接字为监听状态
-	    if (listen(s_server, SOMAXCONN) < 0) {
-	        cout << "ERR" << endl;
-	        WSACleanup();
-	    } 
-	    //接受连接请求
-	    len = sizeof(SOCKADDR);
-	    for (int i = 0; i < B; i++) {
-	    	s_accept[i] = accept(s_server, (SOCKADDR *)&accept_addr, &len);
-		    if (s_accept[i] == SOCKET_ERROR) {
-		        cout << "ERR" << endl;
-		        WSACleanup();
-		        return 0;
-		    }
-		}
-	    return 0;
-	}
-	void close() {
-	    closesocket(s_server);
-	    closesocket(s_accept[0]);
-	    //释放DLL资源
-	    WSACleanup();
-	}
-	std::string rec(int x) {
-		recv_len = recv(s_accept[x], recv_buf, 100, 0);
-	    if (recv_len < 0) {
-	        cout << "ERR" << endl;
-			return "haha";
-	    } else {
-	        std::string an = recv_buf;
-	        return an;
-	    }
-	}
-	int sent(int x, std::string s) {
-	    int send_len = send(s_accept[x], s.c_str(), 100, 0);
-	    std::cout << "senting "+s << '\n';
-	    if (send_len < 0) {
-	        cout << "ERR" << endl;
-	        return ERR;
-	    }
-	}
-} a;
-void give() {
-	int cnt = 0;
-	for (int i = 0; i < B; i++) {
-		if (cnt >= B/2) cha[i] = 0;
-		else cha[i] = rand() % 2;
-		cnt += cha[i];
-		a.sent(i, std::string() + char('0' + cha[i]));
-	}
-	
+using twt = std::vector<int>;
+twt all, wolves, now;
+int die[105], cha[105];
+
+// Tools
+std::string to_string(twt x) {
+	std::string an;
+	for (int c : x) an = an + to_string(c) + " ";
+	return an; 
 }
-void night() {
-	for (int i = 0; i < B; i++) a.sent(i, std::string("nigh"));
-	static int cnt[20];
-	for (int i = 0; i < B; i++) cnt[i] = 0;
-	for (int i = 0; i < B; i++) {
-		if (!cha[i] || die[i]) {
-			continue;
-		}
-		a.sent(i, "y");
-		std::string an = a.rec(i);
-		int x = an[0]-'0'-1;
-		cnt[x] ++;
+int to_int(std::string x) {
+	int an = 0;
+	for (char c : x) an = an * 10 + c - '0';
+	return an;
+}
+int doDis(int x, const twt &chas, const twt &cnt) {
+	if (cnt[_Wolf] >= B/2)  return _Villager;
+	return chas[rand()%chas.size()];
+}
+void distri() {
+	twt chas = {_Villager, _Wolf};
+	twt cnt = {0, 0};
+	for (int i = 1; i <= B; i++) {
+		int c = doDis(i, chas, cnt);
+		cs.sent(i, to_string(c));
+		cnt[c] ++, cha[i] = c;
+		if (c == _Wolf) wolves.push_back(i);
 	}
-	int maxj = 0;
-	for (int i = 0; i < B; i++)
-		if (cnt[i] > cnt[maxj]) maxj = i;
-	die[maxj] = 1;
-	for (int i = 0; i < B; i++) a.sent(i, std::string("dead ")+char(maxj+'0'+1));
+}
+void tele(twt a, std::string s) {
+	std::cout << "sending : " << to_string(a) << ' ' << s << '\n';
+	for (int c : a) cs.sent(c, s);
+}
+twt _vote(twt a) {
+	twt cnt(all.size()+1, 0), ans;
+	for (int c : a) {
+		int r = to_int(cs.rec(c));
+		cnt[r] ++;
+		std::cout << c << '\n';
+	}
+	int mx = *std::max_element(cnt.begin()+1, cnt.end());
+	for (int i = 1; i <= B; i++) 
+		if (cnt[i] == mx) ans.push_back(i);
+	return ans;
+}
+int vote(twt a) {
+	twt cur = _vote(a);
+	while (cur.size() > 1) {
+		tele(a, std::string("Please vote again in ") + to_string(cur));
+		cur = _vote(a);
+	}
+	tele(a, "ok");
+	return cur[0];
+}
+void erase(int x) {
+	int k = 0;
+	for (int i = 0; i < (int)now.size(); i++)
+		if (now[i] == x) k = i;
+	now.erase(now.begin()+k); 
+	k = -1;
+	for (int i = 0; i < (int)wolves.size(); i++)
+		if (wolves[i] == x) k = i;
+	if (k != -1) wolves.erase(wolves.begin()+k);
+}
+void toDie(int x) {
+	die[x] = 1;
+	tele(all, to_string(x) + " dead");
+	erase(x);
+}
+void talk(twt x) {
+	for (int c : x) {
+		cs.sent(c, "you");
+		std::string r = cs.rec(c);
+		tele(all, to_string(c) + " says : " + r);
+	}
+}
+// end Tools
+
+
+// Characters
+void doWolf() {	toDie(vote(wolves)); }
+// end Characters
+
+
+// Days
+void night() { 
+	tele(all, "night");
+	doWolf(); 
 }
 void day() {
-	for (int j = 0; j < B; j++) a.sent(j, "day");
-	for (int i = 0; i < B; i++) {
-		if (die[i]) continue;
-		a.sent(i, "-");
-		std::string mes = a.rec(i);
-		for (int j = 0; j < B; j++) a.sent(j, mes+char(i+'0'));
-	} 
-	static int cnt[20];
-	for (int i = 0; i < B; i++) cnt[i] = 0;
-	for (int i = 0; i < B; i++) {
-		if (die[i]) continue;
-		std::string x = a.rec(i);
-		int k = x[0]-'0'-1;
-		cnt[k] ++;
-	}
-	int maxj = 0;
-	for (int i = 0; i < B; i++)
-		if (cnt[i] > cnt[maxj]) maxj = i;
-	die[maxj] = 1;
-	for (int i = 0; i < B; i++) a.sent(i, std::string("dead ")+char(maxj+'0'+1));
+	tele(all, "day");
+	talk(now);
+	toDie(vote(now));
 }
 bool check() {
 	int cnt = 0;
-	for (int i = 0; i < B; i++) cnt += !die[i] && cha[i];
-	if (cnt == 0) {
-		for (int j = 0; j < B; j++) a.sent(j, "pm");
-		return true;
-	}
+	for (int i = 1; i <= B; i++) cnt += (cha[i] == _Villager && !die[i]);
+	if (!cnt) return tele(all, std::string("&Wolves win!")), 1;
 	cnt = 0;
-	for (int i = 0; i < B; i++) cnt += !die[i] && !cha[i];
-	if (cnt == 0) {
-		for (int j = 0; j < B; j++) a.sent(j, "lr");
-		return true;
-	}
-	return false;
+	for (int i = 1; i <= B; i++) cnt += (cha[i] == _Wolf && !die[i]);
+	if (!cnt) return tele(all, std::string("&Villagers win!")), 1;
+	return 0;
 }
+
 int main() {
-	srand(time(0));
-	std::cout << "num?\n> ";
+	std::cout << "How many players?\n> ";
 	std::cin >> B;
-	std::cout << "waiting for players...\n" ;//<< std::endl;
-	a.init(1234);
-	for (int i = 0; i < B; i++) std::cout << i, a.sent(i, std::string()+char(B+'0'));
-	for (int i = 0; i < B; i++) std::cout << i, a.sent(i, std::string()+char(i+1+'0'));
-	give();
-	while (1) {
-		night();
-		if (check()) break;
-		day();
-		if (check()) break;
+	for (int i = 1; i <= B; i++) all.push_back(i);
+	now = all;
+	system("cls");
+	std::cout << "Waiting for players...\n";
+	cs.init(1234, B);
+	system("cls");
+	std::cout << "Game is running!";
+	tele(all, to_string(B));
+	for (int i = 1; i <= B; i++) cs.sent(i, to_string(i));
+	distri();
+	tele(wolves, to_string(wolves));
+	for (int i = 1; ; i++) {
+		night(); if (check()) break;
+		day(); if (check()) break;
 	}
+	system("pause");
 }
